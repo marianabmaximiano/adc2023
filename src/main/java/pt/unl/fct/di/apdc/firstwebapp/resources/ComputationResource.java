56bd9e4d581ecb0576d71e8f1cc16606107685a4
marianabmaximiano <91958046+marianabmaximiano@users.noreply.github.com>
2023-04-14 00:30:31 +0100

    commit

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
@@ -1,17 +1,30 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
+import java.io.IOException;
+import java.net.URI;
 import java.text.DateFormat;
+
 import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Instant;
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.google.cloud.tasks.v2.AppEngineHttpRequest;
+import com.google.cloud.tasks.v2.CloudTasksClient;
+import com.google.cloud.tasks.v2.HttpMethod;
+import com.google.cloud.tasks.v2.QueueName;
+import com.google.cloud.tasks.v2.Task;
 import com.google.gson.Gson;
+import com.google.protobuf.Timestamp;
 
 
 @Path("/utils")
@@ -28,9 +41,14 @@ public class ComputationResource {
 	@GET
 	@Path("/hello")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response hello() {
-		LOG.fine("Saying hello!!");
-		return Response.ok().entity("Hello human! I hope you are having a fine day.").build();
+	public Response hello() throws IOException {
+		try {
+			throw new IOException("UPS");
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Exception on Method/hello", e);
+			return Response.temporaryRedirect(URI.create("/error/500.html")).build();
+		}
+		
 	}
 	
 	@GET
@@ -40,4 +58,37 @@ public class ComputationResource {
 		LOG.fine("Replying to date request.");
 		return Response.ok().entity(g.toJson(fmt.format(new Date()))).build();
 	}
+	
+	@GET
+	@Path("/compute")
+	public Response triggerExecuteComputeTask() throws IOException {
+	String projectId = "firm-dimension-379414";
+	String queueName = "Default";
+	String location = "europe-west6";
+	LOG.log(Level.INFO, projectId + " :: " + queueName + " :: " + location );
+	try (CloudTasksClient client = CloudTasksClient.create()) { String queuePath = QueueName.of(projectId, location,
+	queueName).toString();
+	             Task.Builder taskBuilder =
+	Task.newBuilder().setAppEngineHttpRequest(AppEngineHttpRequest.newBuilder() .setRelativeUri("/rest/utils/compute").setHttpMethod(HttpMethod.POST)
+	.build());
+	taskBuilder.setScheduleTime(Timestamp.newBuilder().setSeconds(Instant.now(Clock.systemUTC()).getEpochSecond()));
+	
+	client.createTask(queuePath, taskBuilder.build()); }
+	return Response.ok().build(); 
+	}
+
+
+	@POST
+	@Path("/compute")
+	public Response executeComputeTask() {
+		LOG.fine("Starting to execute computation tasks");
+		try {
+				Thread.sleep(60*1000*10);
+		} catch (Exception e) {
+			LOG.logp(Level.SEVERE, this.getClass().getCanonicalName(), "executeComputeTask", "An exception has occurred", e);
+			return Response.serverError().build();
+		}
+		return Response.ok().build();
+	}
+
 }
